fdab2d1892e5e4827576e07e9b0c8087188f441e
Sofia Suvercase <ssuvercase@everfi.com>
2019-11-04 17:32:02 -0300

    Ahorcado con la ayuda de Harold y Diego

--- tp9-arrays/src/ElAhorcado.java
+++ tp9-arrays/src/ElAhorcado.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ElAhorcado {
@@ -7,33 +8,71 @@ public class ElAhorcado {
 		
 		Scanner teclado = new Scanner(System.in);
 		String jugadores[] = new String [2];
-		
-		String palabra = " ";
-		String palabra2 = " ";
+		String palabraIngresada [] = new String [4];
+		int vidasJugador1, vidasJugador2 = 6;
 		
 		
 		System.out.println("Bienvenido al juego del ahorcado!");
 		
-			
+			//ingreso de nombres
 			System.out.println("Ingrese el nombre del jugador 1: ");
-			
 			jugadores[0] = teclado.next();
 			
 			System.out.println("Ingrese el nombre del jugador 2: ");
 			jugadores[1] = teclado.next();
 			System.out.println("Ya estamos listos para empezar!");
 			
+			//ingreso de palabras
+			
 			System.out.println(jugadores[0] + ", Ingrese una palabra:");
-			palabra = teclado.next();
+			palabraIngresada[0] = teclado.next().toUpperCase();
+			palabraIngresada[1] = "_ ".repeat(palabraIngresada[0].length());
+			System.out.println(palabraIngresada[1]);
 			
 			System.out.println(jugadores[1] + ", Ingrese una palabra:");
-			palabra2 = teclado.next();
+			palabraIngresada[2] = teclado.next().toUpperCase();
+			palabraIngresada[3] = "_ ".repeat(palabraIngresada[2].length());
+			System.out.println(palabraIngresada[3]);
 			
-			if(jugadores[0]){
-				System.out.println("El jugador " + jugadores[0] + " comienda la partida!");
+			
+			double empiezaLaPartida = Math.floor(Math.random() * 2);
+			System.out.println(empiezaLaPartida);
+			
+			if(empiezaLaPartida == 0.0){
+				System.out.println("El jugador " + jugadores[0] + " comienza la partida!");
+				
+				palabraIngresada = new String[palabraIngresada[1].length()];
+				
+				for(int i = 0; i<palabraIngresada[0].length(); i++) {
+					System.out.println(palabraIngresada[3]);
+					char charIngresado = teclado.next().charAt(0);
+					
+//					if()
+
+					
+
+				}
+				
+				
 			}else{
-				System.out.println("El jugador " + jugadores[1] + " comienda la partida!");
+				System.out.println("El jugador " + jugadores[1] + " comienza la partida!");
+				for(int i = 0; i<palabraIngresada[2].length(); i++) {
+					System.out.println(palabraIngresada[1]);
+				}
+				
 			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			
 		
